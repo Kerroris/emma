759079cf17fb6b4143c7d41038caed357853ea73
hector <cervaya.003@gmail.com>
2025-02-12 00:04:26 -0600

    registro modificado

--- src/app/page/registro/registro.page.ts
+++ src/app/page/registro/registro.page.ts
@@ -13,6 +13,9 @@ export class RegistroPage implements OnInit {
   registerForm: FormGroup;
   loading!: HTMLIonLoadingElement;
   showPassword: boolean = false;
+  
+  birthDateError: boolean = false;
+  birthDateValid: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -20,35 +23,85 @@ export class RegistroPage implements OnInit {
     private loadingController: LoadingController,
     private alertController: AlertController
   ) {
-    this.registerForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      fullName: ['', [Validators.required]],
-      username: ['', [Validators.required, Validators.pattern(/^\S*$/)]],
-      birthDate: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required]]
-    });
-
-    // Actualizamos la validación de las contraseñas cada vez que el formulario cambia
-    this.registerForm.valueChanges.subscribe(() => {
-      this.passwordsMatch(this.registerForm);
-    });
+    this.registerForm = this.formBuilder.group(
+      {
+        email: [
+          '',
+          [Validators.required, Validators.email, Validators.pattern(/^\S*$/)],
+        ],
+        fullName: ['', [Validators.required]], // Permitido  espacios
+        username: ['', [Validators.required, Validators.pattern(/^\S*$/)]],
+        birthDate: ['', [Validators.required]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(/^(?=.*[A-Z])(?=.*\d).*$/), // 1 mayúscula, 1 número y sin espacios
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validator: this.passwordsMatch }
+    );
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // al iniciar hace q el imput de nombre lo transforme a Mayusculas
+    this.registerForm.get('fullName')?.valueChanges.subscribe((value) => {
+      if (value) {
+        this.registerForm
+          .get('fullName')
+          ?.setValue(value.toUpperCase(), { emitEvent: false });
+      }
+    });
+    this.validateBirthDate();
+  }
 
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
 
-  // Validación de las contraseñas
+  removeSpaces(field: string) {
+    let value = this.registerForm.get(field)?.value;
+    if (value) {
+      this.registerForm
+        .get(field)
+        ?.setValue(value.replace(/\s/g, ''), { emitEvent: false });
+    }
+  }
+
+  validateBirthDate() {
+    const birthDate = this.registerForm.get('birthDate')?.value;
+
+    if (!birthDate) {
+      this.birthDateError = true;
+      this.birthDateValid = false;
+    } else {
+      this.birthDateError = false;
+      this.birthDateValid = true;
+    }
+  }
+
   passwordsMatch(form: FormGroup) {
     const password = form.get('password')?.value;
     const confirmPassword = form.get('confirmPassword')?.value;
-    if (password === confirmPassword) {
-      form.get('confirmPassword')?.setErrors(null);
-    } else {
+
+    if (!password || !confirmPassword) return;
+
+    // Verificar si las contraseñas coinciden
+    if (password !== confirmPassword) {
       form.get('confirmPassword')?.setErrors({ mismatch: true });
+    } else {
+      form.get('confirmPassword')?.setErrors(null); // Limpiar el error de mismatch
+    }
+
+    // Verificar si la contraseña tiene al menos 8 caracteres, una mayúscula y un número
+    const strongPasswordRegex = /^(?=.*[A-Z])(?=.*\d).*$/;
+    if (password.length < 8 || !strongPasswordRegex.test(password)) {
+      form.get('password')?.setErrors({ weakPassword: true });
+    } else {
+      form.get('password')?.setErrors(null); // Limpiar el error de weakPassword
     }
   }
 
@@ -56,6 +109,11 @@ export class RegistroPage implements OnInit {
     if (this.registerForm.invalid) {
       return;
     }
+
+    // let password = this.registerForm.get('password')?.value;
+    // let email = this.registerForm.get('email')?.value;
+    // let username = this.registerForm.get('username')?.value;
+    // console.log(password, email, username);
 
     this.loading = await this.loadingController.create({
       message: 'Registrando...',
